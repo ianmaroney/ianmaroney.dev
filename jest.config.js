0d496007e55637403ad30f733e53a68f8dd57cda
ianmaroney <ian.maroney@gmail.com>
2021-03-04 22:55:44 -0500

    Initial commit of `tests` branch

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>/tests/setup/index.js'],
+  testPathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/'
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/config': '<rootDir>/next.config.js',
+    '^@/data': '<rootDir>/src/data',
+    '^@/hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@/layouts/(.*)$': '<rootDir>/src/components/layouts/$1',
+    // '^@/modules/form': '<rootDir>/src/components/modules/content',
+    '^@/modules/(.*)$': '<rootDir>/src/components/modules/$1',
+    // '^@/partials/form': '<rootDir>/tests/mocks/formMock.js',
+    '^@/partials/(.*)$': '<rootDir>/src/components/partials/$1',
+    '^@/templates/(.*)$': '<rootDir>/src/components/templates/$1',
+    '^@/util': '<rootDir>/src/util',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/tests/mocks/fileMock.js',
+    '\\.(scss)$': '<rootDir>/tests/mocks/styleMock.js'
+  }
+}
---
+++ tests/index.test.js
@@ -0,0 +1,46 @@
+import preloadAll from 'jest-next-dynamic'
+import { getPage } from 'next-page-tester'
+import { screen } from '@testing-library/react'
+
+beforeAll(async () => {
+  await preloadAll()
+})
+
+describe('Pages render', () => {
+  it('renders home page', async () => {
+    const { render } = await getPage({
+      route: '/'
+    })
+
+    render()
+    expect(await screen.findByText('Hey, I’m Ian!', { exact: false })).toBeInTheDocument()
+  })
+
+  it('renders experience page', async () => {
+    const { render } = await getPage({
+      route: '/experience'
+    })
+
+    render()
+    expect(await screen.findByText('Truth Collective, LLC')).toBeInTheDocument()
+  })
+
+  it('renders works page', async () => {
+    const { render } = await getPage({
+      route: '/works'
+    })
+
+    render()
+    expect(await screen.findByText('Selected works')).toBeInTheDocument()
+  })
+
+  it('renders contact page', async () => {
+    const { render } = await getPage({
+      route: '/contact'
+    })
+
+    await render()
+
+    expect(screen.getByText('How to reach me')).toBeInTheDocument()
+  })
+})

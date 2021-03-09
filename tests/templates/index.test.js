92feadf29c372a38a5a225bd2701c63966fba132
ianmaroney <ian.maroney@gmail.com>
2021-03-08 20:58:32 -0500

    Additional template tests

--- tests/templates/index.test.js
+++ tests/templates/index.test.js
@@ -7,28 +7,42 @@ import Primary from '@/templates/primary'
 import data from '../data'
 
 describe('Templates render', () => {
-  // it('null Primary', async () => {
-  //   render(<Primary />)
-  //   expect(screen.queryByText(/\w/)).toBeNull()
-  // })
-  //
-  // it('null Primary: page', async () => {
-  //   render(<Primary page={data.moduleGroupsPage} />)
-  //   expect(screen.queryByText(/\w/)).toBeNull()
-  // })
-  //
-  // it('null Primary: globals', async () => {
-  //   render(<Primary globals={data.metaGlobals} />)
-  //   expect(screen.queryByText(/\w/)).toBeNull()
-  // })
-  //
-  // it('null Primary: menus', async () => {
-  //   render(<Primary menus={Object.assign({}, { main: data.navNavItems })} />)
-  //   expect(screen.queryByText(/\w/)).toBeNull()
-  // })
+  it('null Primary', async () => {
+    render(<Primary />)
+    expect(screen.queryByText(/\w/)).toBeNull()
+  })
 
-  it('null Primary: chidren', async () => {
-    render(<Primary page={data.moduleGroupsPage} globals={data.metaGlobals} menus={Object.assign({}, { main: data.navNavItems })}><p>{data.children}</p></Primary>)
+  it('null Primary: page', async () => {
+    render(<Primary page={data.moduleGroupsPage} />)
+    expect(screen.queryByText(/\w/)).toBeNull()
+  })
+
+  it('null Primary: children', async () => {
+    render(<Primary><p>{data.children}</p></Primary>)
+    expect(screen.queryByText(/\w/)).toBeNull()
+  })
+
+  it('null Primary: globals', async () => {
+    render(<Primary globals={data.metaGlobals} />)
+    expect(screen.queryByText(/\w/)).toBeNull()
+  })
+
+  it('null Primary: menus', async () => {
+    render(<Primary menus={Object.assign({}, { main: data.navNavItems })} />)
+    expect(screen.queryByText(/\w/)).toBeNull()
+  })
+
+  it('null Primary: page + children', async () => {
+    const page = Object.assign({}, data.moduleGroupsPage)
+    render(<Primary page={page}><p>{data.children}</p></Primary>)
+    expect(screen.getByText(data.children)).toBeInTheDocument()
+  })
+
+  it('null Primary: page + globals + menus + children', async () => {
+    const page = Object.assign({}, data.moduleGroupsPage)
+    const globals = Object.assign({}, data.metaGlobals)
+    const menus = Object.assign({}, { main: data.navNavItems })
+    render(<Primary page={page} globals={globals} menus={menus}><p>{data.children}</p></Primary>)
     expect(screen.getByText(data.children)).toBeInTheDocument()
   })
 })

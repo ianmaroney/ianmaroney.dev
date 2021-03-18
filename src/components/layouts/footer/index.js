2adb1a308946853b85e290e20250f17a56e6eb82
ianmaroney <ian.maroney@gmail.com>
2021-03-17 20:49:18 -0400

    Initial commit of the `accessibility` branch
    • `role` attributes
    • `id` attributes
    • `aria-live` attributes
    • `aria-relevant` attributes
    • `aria-labeledby` attributes
    • menu toggle status text
    • Some <div /> tags converted to more appropriate <article /> tags

--- src/components/layouts/footer/index.js
+++ src/components/layouts/footer/index.js
@@ -2,7 +2,7 @@ import styles from './index.module.scss'
 
 const Footer = (props) => {
   return (
-    <footer className={styles.footer}>
+    <footer className={styles.footer} role='contentinfo'>
       <p>✌️</p><p className='soft'>&copy;{new Date().getFullYear()} Ian Maroney</p>
     </footer>
   )
--- src/components/layouts/main/index.js
+++ src/components/layouts/main/index.js
@@ -5,7 +5,7 @@ import styles from './index.module.scss'
 const Main = ({ page }) => {
   if (page && page.moduleGroups) {
     return (
-      <main className={`${styles.main} ${page.slug}`}>
+      <main className={`${styles.main} ${page.slug}`} role='main' aria-live='polite' aria-relevant='all'>
         <div className='container'>
           <ModuleGroups page={page} />
         </div>

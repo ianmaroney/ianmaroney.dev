91d795101b9557129ca8c677f8b25caa5f3509b2
ianmaroney <ian.maroney@gmail.com>
2022-03-16 00:28:48 -0400

    Updates to meta
    Remove custom _document and move static meta to <Meta />

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ module.exports = {
   experimental: {
     outputStandalone: true
   },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en'
+  }
 }
--- src/components/partials/meta/index.js
+++ src/components/partials/meta/index.js
@@ -12,7 +12,18 @@ const Meta = ({ page, globals }) => {
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' key='viewport' />
         <title key='title'>{`${page.title}${globals && globals.titleExtras ? ` - ${globals.titleExtras.join(' - ')}` : ''}`}</title>
-        <meta name='robots' content='index, follow' key='robots' />
+        <meta name='robots' content='noindex, nofollow' key='robots' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' key='gfont-apis' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' key='gfont-static' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' key='icon-180x180' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/images/favicon-32x32.png' key='icon-32x32' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/images/favicon-16x16.png' key='icon-16x16' />
+        <link rel='manifest' href='/site.webmanifest' key='webmanifest' />
+        <link rel='mask-icon' href='/images/safari-pinned-tab.svg' color='#333333' key='mask-icon' />
+        <link rel='shortcut icon' href='/favicon.ico' key='favicon' />
+        <meta name='msapplication-TileColor' content='#ffffff' key='ms-tileColor' />
+        <meta name='msapplication-config' content='/browserconfig.xml' key='ms-config' />
+        <meta name='theme-color' content='#ffffff' key='theme-color' />
       </Head>
     )
   }
